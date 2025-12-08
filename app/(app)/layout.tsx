aaac4902674a18d2b0693e089b790ca37f250d8f
Jared James <jaredjames.personal@gmail.com>
2025-12-09 08:42:24 +1100

    fix: add scrolling and overflow styles to layout container

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -64,7 +64,7 @@ export default async function AppLayout({
       />
 
       <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
-        <div>{children}</div>
+        <div className="flex-1 overflow-y-auto scroll-smooth">{children}</div>
       </main>
     </div>
   );
